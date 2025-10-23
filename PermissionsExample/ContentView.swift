a3487dd133a9a85858537cf293c68cbe9c65036c
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-10-23 19:19:05 +0200

    Initial Commit

---
+++ PermissionsExample/ContentView.swift
@@ -0,0 +1,142 @@
+//
+//  ContentView.swift
+//  PermissionsExample
+//
+//  Created by Tim on 23.10.25.
+//
+
+import SwiftUI
+import Permissions
+import Combine
+import CoreBluetooth
+struct ContentView: View {
+    @StateObject var permissionsManager = PermissionsManager.shared
+    @State var permissionStates: [PermissionType: Bool] = [:]
+    @State var selectedStates: Set<PermissionType> = []
+    @State var showSheet = false
+    @State var isRefreshing = false
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    if selectedStates == Set(PermissionType.allCases) {
+                        Button("Deselect All") {
+                            withAnimation {
+                                selectedStates = []
+                            }
+                        }
+                    } else {
+                        Button("Select All") {
+                            withAnimation {
+                                selectedStates = Set(PermissionType.allCases)
+                            }
+                        }
+                    }
+                    ForEach(PermissionType.allCases.sorted(by: { $0.rawValue.localizedCompare($1.rawValue) == .orderedAscending }), id: \.rawValue) { type in
+                        Button(action: {
+                            if selectedStates.contains(type) {
+                                selectedStates.remove(type)
+                            } else {
+                                selectedStates.insert(type)
+                            }
+                        }) {
+                            HStack {
+                                Image(systemName: type.sfSymbol)
+                                    .frame(width: 30)
+                                    .foregroundStyle(Color(uiColor: .label))
+                                VStack(alignment: .leading) {
+                                    Text(type.localizedTitle)
+                                        .bold()
+                                        .foregroundStyle(Color(uiColor: .label))
+                                    let granted = permissionStates[type] ?? false
+                                    HStack(spacing: 5) {
+                                        Image(systemName: granted ? "checkmark.circle.fill" : "xmark.circle.fill")
+                                        Text(granted ? permissionsManager.localizationProvider.grantedTitle : permissionsManager.localizationProvider.notGrantedTitle)
+                                    }
+                                    .font(.caption)
+                                    .foregroundStyle(granted ? .green : .red)
+                                }
+                                Spacer()
+                                if selectedStates.contains(type) {
+                                    Image(systemName: "checkmark")
+                                        .foregroundStyle(.tint)
+                                }
+                            }
+                            .contentShape(.rect)
+                        }
+                        .tag(type)
+                    }
+                } header: {
+                    HStack {
+                        Text("Select Permission Types")
+                        Spacer()
+                        if isRefreshing {
+                            ProgressView()
+                        }
+                    }
+                } footer: {
+                    Text("Select all Permission Types you want to Request, once you are Done selecting, press \"Open Sheet\" to open the Permission Sheet")
+                }
+            }
+            .safeAreaInset(edge: .bottom) {
+                Button(action: {
+                    showSheet.toggle()
+                }) {
+                    HStack {
+                        Spacer()
+                        Text("Open Sheet")
+                            .bold()
+                        Spacer()
+                    }
+                    .padding(10)
+                }
+                .borderedProminent()
+                .padding()
+                .disabled(selectedStates.isEmpty)
+            }
+            .permissionsSheet(isPresented: $showSheet, permissions: selectedStates)
+        }
+        .navigationViewStyle(.stack)
+        .background(.ultraThinMaterial)
+        .onChange(of: showSheet) { _ in
+            Task { await reloadStates() }
+        }
+        .onAppear {
+            refreshLoop()
+        }
+    }
+    func reloadStates() async {
+        if !showSheet {
+            withAnimation {
+                isRefreshing = true
+            }
+            try? await Task.sleep(nanoseconds: 1_000_000_000)
+            permissionStates = await permissionsManager.getPermissionStates(for: Set(PermissionType.allCases))
+            withAnimation {
+                isRefreshing = false
+            }
+        }
+    }
+    func refreshLoop() {
+        Task {
+            await reloadStates()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                refreshLoop()
+            }
+        }
+    }
+}
+
+extension View {
+    func borderedProminent() -> some View {
+        if #available(iOS 26.0, *) {
+            return self.buttonStyle(.glassProminent)
+        } else {
+            return self.buttonStyle(.borderedProminent)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PermissionsExample/PermissionsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PermissionsExampleApp.swift
+//  PermissionsExample
+//
+//  Created by Tim on 23.10.25.
+//
+
+import SwiftUI
+
+@main
+struct PermissionsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
